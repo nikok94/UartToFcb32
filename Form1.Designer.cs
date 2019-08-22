0c8869845fc9f140e23b3a0ca59f94f11caaaf39
Дмитрий Кокин <19nikok941@gmail.com>
2019-08-22 13:10:17 +0300

    test_commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace UartToFcb32
+{
+    partial class UartToFCB
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LablePortOpen = new System.Windows.Forms.Label();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.dataBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите COM-порт";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "57600",
+            "115200",
+            "230400",
+            "460800",
+            "560800",
+            "921600",
+            "1036800"});
+            this.comboBox2.Location = new System.Drawing.Point(111, 152);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Скорость передачи (boad/s)";
+            // 
+            // LablePortOpen
+            // 
+            this.LablePortOpen.AutoSize = true;
+            this.LablePortOpen.Location = new System.Drawing.Point(234, 157);
+            this.LablePortOpen.Name = "LablePortOpen";
+            this.LablePortOpen.Size = new System.Drawing.Size(0, 13);
+            this.LablePortOpen.TabIndex = 9;
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Location = new System.Drawing.Point(311, 152);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(75, 23);
+            this.buttonWrite.TabIndex = 11;
+            this.buttonWrite.Text = "Записать";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.ButtonWrite_Click);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(415, 152);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 23);
+            this.buttonRead.TabIndex = 11;
+            this.buttonRead.Text = "Прочитать";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.ButtonRead_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // addressBox
+            // 
+            this.addressBox.Location = new System.Drawing.Point(311, 108);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(92, 20);
+            this.addressBox.TabIndex = 12;
+            this.addressBox.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // dataBox
+            // 
+            this.dataBox.Location = new System.Drawing.Point(415, 108);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(92, 20);
+            this.dataBox.TabIndex = 12;
+            this.dataBox.TextChanged += new System.EventHandler(this.DataBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(308, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Адрес";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(412, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Данные";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Записать массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(302, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Прочитать массив";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // UartToFCB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataBox);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.LablePortOpen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "UartToFCB";
+            this.Text = "UartToFCB_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LablePortOpen;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.TextBox dataBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+using UartToFcb32.Properties;
+
+namespace UartToFcb32
+{
+    public partial class UartToFCB : Form
+    {
+        public UartToFCB()
+        {
+            InitializeComponent();
+            comboBox2.SelectedIndex = Convert.ToInt32(Settings.Default["BaudRate"]);
+            addressBox.Text = Settings.Default["Address"].ToString();
+            dataBox.Text = Settings.Default["Data"].ToString();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+            comboBox1.SelectedIndex = Convert.ToInt32(Settings.Default["ComPortNum"]);
+            timer1.Start();
+
+        }
+
+        String LastName;
+        static SerialPort Serial;
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indx = comboBox1.SelectedIndex; ;
+            Settings.Default["ComPortNum"] = indx;
+            Settings.Default.Save();
+            String Name = ((string)comboBox1.SelectedItem);
+
+
+            if (LastName == null)
+            {
+                LastName = Name;
+                Serial = new SerialPort(Name, 115200, System.IO.Ports.Parity.None, 8, StopBits.One);
+                if (Serial.IsOpen)
+                {
+                    LablePortOpen.Text = ("Порт недоступен");
+                }
+                else
+                {
+                    Serial.Open();
+                    LablePortOpen.Text = ("Порт открыт");
+                    //Serial.DataReceived += SerialPort_DataReceived;
+                }
+            }
+            else
+            {
+                if (!String.Equals(LastName, Name))
+                {
+                    Serial.Close();
+                    Serial = new SerialPort(Name, Convert.ToInt32((string)comboBox2.SelectedItem), System.IO.Ports.Parity.None, 8, StopBits.One);
+                    if (Serial.IsOpen)
+                    {
+                        LablePortOpen.Text = ("Порт недоступен");
+                    }
+                    else
+                    {
+                        Serial.Open();
+                        LablePortOpen.Text = ("Порт открыт");
+                        // Serial.DataReceived += SerialPort_DataReceived;
+                    }
+
+                }
+            }
+
+
+
+        }
+
+        private void ComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indx = comboBox2.SelectedIndex; ;
+            Settings.Default["BaudRate"] = indx;
+            Settings.Default.Save();
+
+            if (!(Serial == null))
+            {
+                Serial.BaudRate = Convert.ToInt32((string)comboBox2.SelectedItem);
+            }
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            char[] symbols = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'x', 'a', 'b', 'c', 'd', 'e', 'f' };
+            string s = addressBox.Text;
+            char[] sn = new char[10];
+            sn[0] = '0';
+            sn[1] = 'x';
+
+            if (s.Length > 10)
+            {
+                if (s.Length == 11)
+                {
+                    for (int i = 2; i < 10; i++)
+                    {
+                        sn[i] = s[i + 1];
+                    }
+                    addressBox.Text = new string(sn.Where(x => symbols.Contains(x)).ToArray());
+
+                }
+                else
+
+                {
+                    addressBox.Text = "0x00000000";
+                }
+
+
+            }
+            if (s.Length < 10)
+            {
+                if (s.Length == 9)
+                {
+                    sn[2] = '0';
+                    for (int i = 2; i < 9; i++)
+                    {
+                        sn[i + 1] = s[i];
+                    }
+                    addressBox.Text = new string(sn.Where(x => symbols.Contains(x)).ToArray());
+                }
+                else
+                {
+                    addressBox.Text = "0x00000000";
+                }
+            }
+            addressBox.Select(10, 10);
+            Settings.Default["Address"] = addressBox.Text;
+            Settings.Default.Save();
+        }
+
+        private void DataBox_TextChanged(object sender, EventArgs e)
+        {
+            char[] symbols = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'x', 'a', 'b', 'c', 'd', 'e', 'f' };
+            string s = dataBox.Text;
+            char[] sn = new char[10];
+            sn[0] = '0';
+            sn[1] = 'x';
+            if (s.Length > 10)
+            {
+                if (s.Length == 11)
+                {
+                    for (int i = 2; i < 10; i++)
+                    {
+                        sn[i] = s[i + 1];
+                    }
+                    dataBox.Text = new string(sn.Where(x => symbols.Contains(x)).ToArray());
+                }
+                else
+                {
+                    dataBox.Text = "0x00000000";
+                }
+            }
+            if (s.Length < 10)
+            {
+                if (s.Length == 9)
+                {
+                    sn[2] = '0';
+                    for (int i = 2; i < 9; i++)
+                    {
+                        sn[i + 1] = s[i];
+                    }
+                    dataBox.Text = new string(sn.Where(x => symbols.Contains(x)).ToArray());
+                }
+                else
+                {
+                    dataBox.Text = "0x00000000";
+                }
+            }
+            dataBox.Select(10, 10);
+            Settings.Default["Data"] = dataBox.Text;
+            Settings.Default.Save();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.Clear();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        public void Write32(SerialPort Port, UInt32 Address, UInt32 Data)
+        {
+            byte comm = 2;
+
+            byte[] AddrByteArray = BitConverter.GetBytes(Address);
+            byte[] DataByteArray = BitConverter.GetBytes(Data);
+            byte[] LenByteArray = BitConverter.GetBytes(0x00000001);
+            byte[] ToPushByteArray = new byte[1 + AddrByteArray.Length + LenByteArray.Length + DataByteArray.Length];
+
+            ToPushByteArray[0] = comm;
+            AddrByteArray.CopyTo(ToPushByteArray, 1);
+            LenByteArray.CopyTo(ToPushByteArray, 1 + AddrByteArray.Length);
+            DataByteArray.CopyTo(ToPushByteArray, 1 + AddrByteArray.Length + LenByteArray.Length);
+
+            Serial.Write(ToPushByteArray, 0, ToPushByteArray.Length);
+
+            int buferSize = Port.BytesToRead;
+            while (buferSize < 1)
+            {
+                buferSize = Port.BytesToRead;
+            }
+
+            byte RdData = Convert.ToByte(Serial.ReadByte());
+
+
+        }
+        public void Write32(SerialPort Port, UInt32 Address, UInt32[] Data, bool inc)
+        {
+            byte comm;
+            if (inc)
+                comm = 3;
+            else
+                comm = 2;
+
+            byte[] AddrByteArray = BitConverter.GetBytes(Address);
+            byte[] DataByteArray = new byte[4 * Data.Length];
+            for (int i = 0; i < DataByteArray.Length / 4; i++)
+            {
+                BitConverter.GetBytes(Data[i]).CopyTo(DataByteArray, i * 4);
+            }
+
+            byte[] LenByteArray = BitConverter.GetBytes(Data.Length);
+            byte[] ToPushByteArray = new byte[1 + AddrByteArray.Length + LenByteArray.Length + DataByteArray.Length];
+
+            ToPushByteArray[0] = comm;
+            AddrByteArray.CopyTo(ToPushByteArray, 1);
+            LenByteArray.CopyTo(ToPushByteArray, 1 + AddrByteArray.Length);
+            DataByteArray.CopyTo(ToPushByteArray, 1 + AddrByteArray.Length + LenByteArray.Length);
+
+            Serial.Write(ToPushByteArray, 0, ToPushByteArray.Length);
+
+            int buferSize = Port.BytesToRead;
+            while (buferSize < 1)
+            {
+                buferSize = Port.BytesToRead;
+            }
+
+            byte RdData = Convert.ToByte(Serial.ReadByte());
+
+        }
+        public static UInt32 Read32(SerialPort Port, UInt32 Address)
+        {
+            byte comm = 0;
+            byte[] AddrByteArray = BitConverter.GetBytes(Address);
+            byte[] LenByteArray = BitConverter.GetBytes(0x00000001);
+            byte[] ToPushByteArray = new byte[1 + AddrByteArray.Length + LenByteArray.Length];
+            ToPushByteArray[0] = comm;
+            AddrByteArray.CopyTo(ToPushByteArray, 1);
+            LenByteArray.CopyTo(ToPushByteArray, 1 + AddrByteArray.Length);
+            Serial.Write(ToPushByteArray, 0, ToPushByteArray.Length);
+
+
+            int buferSize = Port.BytesToRead;
+            while (buferSize < 4)
+            {
+                buferSize = Port.BytesToRead;
+            }
+
+            byte[] RdData = new byte[4];
+            for (int i = 0; i < RdData.Length; i++)
+            {
+                RdData[i] = Convert.ToByte(Serial.ReadByte());
+            }
+
+            return BitConverter.ToUInt32(RdData, 0);
+
+
+        }
+        public static UInt32[] Read32(SerialPort Port, UInt32 Address, UInt32 DataLen, bool inc)
+        {
+            byte comm;
+            if (inc)
+                comm = 1;
+            else
+                comm = 0;
+      
+            byte[] AddrByteArray = BitConverter.GetBytes(Address);
+            byte[] LenByteArray = BitConverter.GetBytes(DataLen);
+            byte[] ToPushByteArray = new byte[1 + AddrByteArray.Length + LenByteArray.Length];
+            ToPushByteArray[0] = comm;
+            AddrByteArray.CopyTo(ToPushByteArray, 1);
+            LenByteArray.CopyTo(ToPushByteArray, 1 + AddrByteArray.Length);
+            Serial.Write(ToPushByteArray, 0, ToPushByteArray.Length);
+      
+            byte[] RdData = new byte[4];
+            UInt32[] RdBuffer = new UInt32[DataLen];
+            int buferSize = Port.BytesToRead;
+            for (int i = 0; i < DataLen; i++)
+            {
+                while (buferSize < 4)
+                {
+                    buferSize = Port.BytesToRead;
+                }
+                for ( int j = 0; j < 4; j++)
+                {
+                    RdData[j] = Convert.ToByte(Serial.ReadByte());
+                }
+                RdBuffer[i] = BitConverter.ToUInt32(RdData, 0);
+      
+            }
+       
+            return RdBuffer;
+      
+      
+        }
+
+        private void ButtonWrite_Click(object sender, EventArgs e)
+        {
+            uint AddrUint = uint.Parse(addressBox.Text.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            uint DataUint = uint.Parse(dataBox.Text.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            Write32(Serial, AddrUint, DataUint);
+        }
+
+        private void ButtonRead_Click(object sender, EventArgs e)
+        {
+            uint AddrUint = uint.Parse(addressBox.Text.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            UInt32 RdData = Read32(Serial, AddrUint);
+            byte[] RdDataByteArray = BitConverter.GetBytes(RdData);
+            byte[] RdDataByteArrayC = new byte[RdDataByteArray.Length];
+            for (int i = 0; i < RdDataByteArray.Length; i++ )
+            {
+                RdDataByteArrayC[i] = RdDataByteArray[RdDataByteArray.Length - 1 - i];
+            }
+
+            string str = BitConverter.ToString(RdDataByteArrayC);
+            dataBox.Text = "0x" + str.Replace("-", ""); ;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            UInt32 AddrUint = uint.Parse(addressBox.Text.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            UInt32[] DataArray = new UInt32[8];
+            for (int i = 0; i < DataArray.Length; i++)
+            {
+                DataArray[i] = Convert.ToUInt32(i * 4);
+            }
+            Write32(Serial, AddrUint, DataArray, true);
+        
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            uint AddrUint = uint.Parse(addressBox.Text.Substring(2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            UInt32[] RdData = Read32(Serial, AddrUint, 8, true);
+        }
+
+    }
+}
